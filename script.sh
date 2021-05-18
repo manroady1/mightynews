0db29f35367a6a0729e7e9d0486fdead407f0165
Manik <gmanik1212@gmail.com>
2021-05-18 19:34:27 +0530

    Initial commit

--- script.sh
+++ script.sh
@@ -5,3 +5,5 @@ git add .
 git commit -m "Initial commit"
 git remote add origin https://github.com/manroady1/mightynews.git
 git push origin master
+
+find /home/manik/Documents/Test -type f -mtime +1 -exec rm -f {} \;
